0e43d16561ae62a78fe2782389490c5905fb5b05
Michael S Lee <110694553+michaelsblee@users.noreply.github.com>
2024-10-31 04:48:45 +0000

    memo v2

--- memoization_dp/burger_fervor_memo.c
+++ memoization_dp/burger_fervor_memo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#define SIZE 10000
 unsigned long long total_calls;
 
 
@@ -11,41 +11,51 @@ int max(int v1, int v2) {
     else
         return v2;
 }
-int solve_t(int m, int n, int t) {
+int solve_t(int m, int n, int t, int memo[]) {
     int first, second;
-    total_calls++;
-    if (t==0)
-        return 0;
+    if (memo[t]!=-2)
+        return memo[t];
+    //total_calls++;
+    if (t==0) {
+        memo[t] = 0;
+        return memo[t];
+    }
     if (t>=m)
-        first = solve_t(m, n, t-m);
+        first = solve_t(m, n, t-m, memo);
     else
         first = -1;
     if (t>=n)
-        second = solve_t(m, n, t-n);
+        second = solve_t(m, n, t-n, memo);
     else
         second = -1;
-    if (first == -1 && second == -1)
-        return -1;
-    else
-        return max(first, second) + 1;
+    if (first == -1 && second == -1) {
+        memo[t] = -1;
+        return memo[t];
+    }
+    else {
+        memo[t] = max(first, second) + 1;
+        return memo[t];
+    }
 }
-
 void solve(int m, int n, int t) {
     int result, i;
-    total_calls = 0;
-    result = solve_t(m, n, t);
+    int memo[SIZE];
+
+    //total_calls = 0;
+    for (i=0; i<=t; i++)
+        memo[i] = -2;
+    result = solve_t(m, n, t, memo);
     if (result >= 0)
         printf("%d\n", result);
     else {
         i = t - 1;
-        result = solve_t(m, n, i);
+        result = solve_t(m, n, i, memo);
         while (result == -1) {
             i--;
-            result = solve_t(m, n, i);
+            result = solve_t(m, n, i, memo);
         }
         printf("%d %d\n", result, t - i);
     }
-    printf("Total calls to solve_t: %llu\n", total_calls);
 }
 
 int main(void) { 
